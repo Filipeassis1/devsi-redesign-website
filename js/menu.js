f8a76b4e64ed5631db8bfbf428957f740a264a1e
Devsi Tecnologia <devsitecnologia@gmail.com>
2025-07-11 15:33:19 -0300

    inicio filtros

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,22 @@
+
 const hamburguer = document.getElementById('hamburguer');
 const menuMobile = document.getElementById('mobile-menu');
+const hamburguerIcon = document.getElementById('hamburguer-icon');
 
 
 hamburguer.addEventListener('click', ()=>{
+
     menuMobile.classList.toggle('active');
+    hamburguerIcon.classList.toggle('active');
+
+
+    if (hamburguerIcon.classList.contains('active')){
+
+        hamburguerIcon.src = "/assets/svgs/close.svg";
+    } else {
+        hamburguerIcon.src =  "/assets/svgs/hamburguer-menu.svg";
+    }
 });
+
+
+
